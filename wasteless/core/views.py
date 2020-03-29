62516d48b2918df0324e0d7c9003e33c6db6bce7
Andy Tosa <andy.tosa98@gmail.com>
2020-03-29 16:54:25 +0300

    Created site template in core app

---
+++ wasteless/core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'core/home.html')
+
+def about(request):
+    return render(request, 'core/about.html')
