b41672d125b9a958ec892a81ba974bd64165bf2d
Andy Tosa <andy.tosa98@gmail.com>
2020-03-29 20:22:18 +0300

    Created Users app
    Added login, register, logout
    Created profile
    Automatic profile creation when registering

--- wasteless/core/models.py
+++ wasteless/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class FoodItem(models.Model):
+    name = models.CharField(max_length=30)
+    quantity = models.PositiveIntegerField()
+    calories = models.PositiveIntegerField()
+    buy_date = models.DateField()
+    exp_date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} ({self.user.username})"
--- wasteless/wasteless/urls.py
+++ wasteless/wasteless/urls.py
@@ -1,4 +1,4 @@
-"""wasteless URL Configuration
+"""django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -11,12 +11,23 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('core/', include('core.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('core.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
