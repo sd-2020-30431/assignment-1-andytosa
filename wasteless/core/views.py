f78128c06c267f5336a2eba6bdca92eb8ee70273
Andy Tosa <andy.tosa98@gmail.com>
2020-03-30 16:00:50 +0300

    Added donation places

--- wasteless/core/views.py
+++ wasteless/core/views.py
@@ -11,8 +11,32 @@ from django.views.generic import (
 )
 from .models import FoodItem
 
+objects = [
+    {
+        'title': 'Red Cross',
+        'content': 'The International Red Cross and Red Crescent Movement is an international humanitarian movement with approximately 97 million volunteers, members and staff worldwide[2] which was founded to protect human life and health, to ensure respect for all human beings, and to prevent and alleviate human suffering. ',
+        'open_hours': 'Monday to Friday, 8AM - 6PM',
+        'imgurl': 'media/food_banks/redcross.jpg'
+    },
+    {
+        'title': 'Salvation Army',
+        'content': 'The Army was founded in 1865 in London by one-time Methodist circuit-preacher William Booth and his wife Catherine as the East London Christian Mission, and can trace its origins to the Blind Beggar tavern. ',
+        'open_hours': 'Monday to Saturday, 10AM - 6PM',
+        'imgurl': 'media/food_banks/salvarmy.png'
+    },
+    {
+        'title': 'Food Bank',
+        'content': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        'open_hours': 'Lorem ipsum dolor, sit - amet',
+        'imgurl': 'media/food_banks/rename.png'
+    }
+]
+
 def home(request):
-    return render(request, 'core/home.html')
+    context = {
+        'objects': objects
+    }
+    return render(request, 'core/home.html', context)
 
 
 def myfood(request):
